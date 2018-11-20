53547483e431902e424561b2278e83f1b6db453e
praathul <44019979+athulpram@users.noreply.github.com>
2018-11-20 13:55:46 +0530

    Add some test cases for map function
    Added test for testing an array of strings

--- test/arrayFunctionTest.js
+++ test/arrayFunctionTest.js
@@ -5,11 +5,21 @@ const square = function(number){
   return number*number;
 }
 
+const stringLength = function(string){
+  return string.length;
+}
+
 const mapTest = function(){
   //Testing map for square function 
   deepEqual(map(square,[]),[]);
-}
+  deepEqual(map(square,[1]),[1]);
+  deepEqual(map(square,[1,2]),[1,4]);
 
+  //Testing map for inputs as string
+  deepEqual(map(stringLength,[]),[]);
+  deepEqual(map(stringLength,["computer"]),[8]);
+  deepEqual(map(stringLength,["computer","abcd"]),[8,4]);
+}
 console.log("testing map functions");
 mapTest();
 console.log("map tests passed");
